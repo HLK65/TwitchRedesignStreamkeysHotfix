c7839ede68fd05b0ed621304c9b62db1a38a2e7e
Lukas <HLK65@users.noreply.github.com>
2019-11-10 18:28:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@
 // @author       hlk
 // @include      https://www.twitch.tv/*
 // @include      https://twitch.tv/*
-// @exclude      https://www.twitch.tv/*/*
-// @exclude      https://twitch.tv/*/*
 // @grant        none
 // ==/UserScript==
 
